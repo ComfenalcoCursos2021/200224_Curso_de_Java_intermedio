433639d85f99b40c2844850d3977831e0d438322
alvaro.obregon <alvaro911>
2024-05-27 21:47:31 -0500

    First

---
+++ WORKSPACE/POO001Laboratorio/src/poo/grupo/mayo/unc/Primera.java
@@ -0,0 +1,12 @@
+package poo.grupo.mayo.unc;
+
+public class Primera {	
+	
+	public static void main(String[] argumentos) {
+		
+		System.out.println(" ---Bienvenida--- ");
+		System.out.println();
+		
+	}
+	
+}
