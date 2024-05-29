da6e245cf6a8d32d6fa6c81b4067d186eeb9442c
alvaro.obregon <alvaro911>
2024-05-28 19:47:22 -0500

    Clase 20240528

--- WORKSPACE/POO001Laboratorio/src/poo/grupo/mayo/unc/Primera.java
+++ WORKSPACE/POO001Laboratorio/src/poo/grupo/mayo/unc/Primera.java
@@ -1,10 +1,30 @@
 package poo.grupo.mayo.unc;
 
+import java.util.Scanner;
+
 public class Primera {	
 	
 	public static void main(String[] argumentos) {
 		
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		short a = 18;
+		int b = 18;
+		long c = 18;
+		float d = 18;
+		double e = 18;
+		
+		Short am = new Short("18");
+		Integer bm = new Integer(18);
+		Long cm = new Long(18);
+		
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println("--------------");
+		
 		System.out.println(" ---Bienvenida--- ");
+		System.out.println();
 		System.out.println();
 		
 	}
